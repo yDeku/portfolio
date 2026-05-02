147e78461a0a2fc4c811753c87f0d7c3ddc9e32b
ydkDev <99555671+yDeku@users.noreply.github.com>
2026-05-02 16:45:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+
+// Discord (copiar)
 const discord = document.getElementById('discord');
 
 discord.addEventListener('click', () => {
   navigator.clipboard.writeText('anakinsky01');
-  discord.innerText = "Copiado!";
+  alert('Discord copiado!');
+});
+
+// WhatsApp (abrir)
+const whatsapp = document.getElementById('whatsapp');
+
+whatsapp.addEventListener('click', () => {
+  window.open('https://wa.me/5500000000000', '_blank');
 });
