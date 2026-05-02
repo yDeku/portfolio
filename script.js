f45be7beb202a232709815b6ce6fdde5b1a63d1e
ydkDev <99555671+yDeku@users.noreply.github.com>
2026-05-02 16:21:39 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const discord = document.getElementById('discord');
+
+discord.addEventListener('click', () => {
+  navigator.clipboard.writeText('Deku#0001');
+  discord.innerText = "Copiado!";
+});
